8940c7440261e369b3570399143ee4ae2ecf72a8
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2020-02-04 20:38:28 -0700

    Update to correct Babel syntax

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+const app = express();
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration)
+
+app.locals.title = 'Palette Picker';
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  response.send('Welcome to Palette Picker!');
+});
+
+export default app;
